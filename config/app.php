139a319a495783c6878bf1fd1231bdb2ee950119
Matth98666 <matthnev@orange.fr>
2023-06-15 17:49:25 +0200

    update

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+$conf = [
+    'app' => [
+      'title'   => 'MVC COUNTRIES'
+    ],
+    'db' => [
+        /* Entrez ici vos identifiants pour se connecter à la base de données */
+        'host'      => 'localhost',
+        'database'  => 'mvc',
+        'user'      => 'root',
+        'password'  => 'Tetedoeuf98!'
+    ]
+];
---
+++ resources/views/layouts/Default.layout.php
@@ -0,0 +1,61 @@
+<?php
+    /*** Default Layout ***/
+    global $conf, $countrylist_active, $regionlist_active;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{pageTitle}</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/mvc.min.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/imgs/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/imgs/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/imgs/favicon-16x16.png">
+    <link rel="manifest" href="assets/imgs/site.webmanifest">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php
+            require_once __DIR__ . '/../partials/header.partial.php';
+            ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <main id="mainContent" class="p-3">
+                {pageContent}
+            </main>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <?php
+            require_once __DIR__ . '/../partials/footer.partial.php';
+            ?>
+        </div>
+    </div>
+</div>
+
+<div class="offcanvas offcanvas-end" data-bs-scroll="false" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" id="mvcOffCanvas" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasRightLabel"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body"></div>
+</div>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/mvc.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/3.0.1/js.cookie.min.js" integrity="sha512-wT7uPE7tOP6w4o28u1DN775jYjHQApdBnib5Pho4RB0Pgd9y7eSkAV1BTqQydupYDB9GBhTcQQzyNMPMV3cAew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+    Cookies.set('name', 'value', { expires: 365 })
+    Cookies.get('name') // => 'value'
+</script>
+
+</body>
+</html>
+
